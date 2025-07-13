206ef5342203540b0bfa01d255d99cb8680448fb
Leonid Pershin <lpershin@syntellect.ru>
2025-07-13 22:15:38 +0300

    Update MigrationTests.cs

--- Tests/InfrastructureTests/MigrationTests.cs
+++ Tests/InfrastructureTests/MigrationTests.cs
@@ -23,6 +23,14 @@ public class MigrationTests
         {
             // Smoke: просто проверяем, что миграции применяются без исключений
             context.Database.Migrate();
+
+            // Assert that the __EFMigrationsHistory table exists
+            var command = context.Database.GetDbConnection().CreateCommand();
+            command.CommandText =
+                "SELECT name FROM sqlite_master WHERE type='table' AND name='__EFMigrationsHistory';";
+            var result = command.ExecuteScalar();
+            Assert.NotNull(result);
+            Assert.Equal("__EFMigrationsHistory", result);
         }
     }
 
@@ -45,6 +53,12 @@ public class MigrationTests
             context.Database.Migrate("0");
             // Снова применяем все миграции
             context.Database.Migrate();
+            var command = context.Database.GetDbConnection().CreateCommand();
+            command.CommandText =
+                "SELECT name FROM sqlite_master WHERE type='table' AND name='__EFMigrationsHistory';";
+            var result = command.ExecuteScalar();
+            Assert.NotNull(result);
+            Assert.Equal("__EFMigrationsHistory", result);
         }
     }
 }
