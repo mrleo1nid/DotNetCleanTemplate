fa0ea237476ef66d5fc71f1293cd661036ca6a16
Leonid Pershin <lpershin@syntellect.ru>
2025-07-22 17:10:53 +0300

    fix

--- Clients/DotNetCleanTemplate.WebClient/Program.cs
+++ Clients/DotNetCleanTemplate.WebClient/Program.cs
@@ -1,8 +1,5 @@
-using System.Net.Http;
-using System.Net.Http.Json;
 using DotNetCleanTemplate.WebClient;
 using DotNetCleanTemplate.WebClient.Configurations;
-using DotNetCleanTemplate.WebClient.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -11,30 +8,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Загрузка конфигурации API
 var settings = new ClientConfig();
 builder.Configuration.Bind(settings);
 builder.Services.AddSingleton(settings);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(settings.Api.BaseUrl) });
-builder.Services.AddScoped<AuthTokenService>();
-builder.Services.AddHttpClient(
-    "Api",
-    client =>
-    {
-        client.BaseAddress = new Uri(settings.Api.BaseUrl);
-    }
-);
-builder.Services.AddTransient<AuthHttpMessageHandler>();
-builder
-    .Services.AddHttpClient(
-        "AuthorizedApi",
-        client =>
-        {
-            client.BaseAddress = new Uri(settings.Api.BaseUrl);
-        }
-    )
-    .AddHttpMessageHandler<AuthHttpMessageHandler>();
-builder.Services.AddMudServices();
 
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
