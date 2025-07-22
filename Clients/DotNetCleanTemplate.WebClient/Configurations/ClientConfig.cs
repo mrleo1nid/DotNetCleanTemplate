95ffeb9a5113ad9c9f519c98e0fdac13d05a71cb
Leonid Pershin <lpershin@syntellect.ru>
2025-07-22 14:20:27 +0300

    fix client

---
+++ Clients/DotNetCleanTemplate.WebClient/Configurations/ClientConfig.cs
@@ -0,0 +1,13 @@
+namespace DotNetCleanTemplate.WebClient.Configurations
+{
+    public class ClientConfig
+    {
+        public ApiSection Api { get; set; } = new();
+    }
+
+    public class ApiSection
+    {
+        public const string SectionName = "Api";
+        public string BaseUrl { get; set; } = string.Empty;
+    }
+}
--- Clients/DotNetCleanTemplate.WebClient/Program.cs
+++ Clients/DotNetCleanTemplate.WebClient/Program.cs
@@ -1,11 +1,40 @@
+using System.Net.Http;
+using System.Net.Http.Json;
 using DotNetCleanTemplate.WebClient;
+using DotNetCleanTemplate.WebClient.Configurations;
+using DotNetCleanTemplate.WebClient.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Загрузка конфигурации API
+var settings = new ClientConfig();
+builder.Configuration.Bind(settings);
+builder.Services.AddSingleton(settings);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(settings.Api.BaseUrl) });
+builder.Services.AddScoped<AuthTokenService>();
+builder.Services.AddHttpClient(
+    "Api",
+    client =>
+    {
+        client.BaseAddress = new Uri(settings.Api.BaseUrl);
+    }
+);
+builder.Services.AddTransient<AuthHttpMessageHandler>();
+builder
+    .Services.AddHttpClient(
+        "AuthorizedApi",
+        client =>
+        {
+            client.BaseAddress = new Uri(settings.Api.BaseUrl);
+        }
+    )
+    .AddHttpMessageHandler<AuthHttpMessageHandler>();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
